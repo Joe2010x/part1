4a03eaf18cf5052768d1cad1ead7686d21992f6c
Dongping Sun <dongping.d.sun@utu.fi>
2021-10-06 22:09:52 +0300

    part 1.3

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -20,7 +20,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.name} {props.ex}
+        {props.part.name} {props.part.exercises}
       </p>
     </div>
   );
@@ -28,19 +28,24 @@ const Part = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises : 10
+}
+  const part2 = {
+    name: "Using props to pass data",
+    exercises : 7
+  }
+  const part3 = {
+    name:"State of a component",
+    exercises : 14}
 
   const Content = () => {
     return (
       <div>
-        <Part name={part1} ex={exercises1} />
-        <Part name={part2} ex={exercises2} />
-        <Part name={part3} ex={exercises3} />
+        <Part part={part1} />
+        <Part part={part2} />
+        <Part part={part3} />
       </div>
     );
   };
@@ -49,7 +54,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
